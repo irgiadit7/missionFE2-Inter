e68720312ecaabfbb581b2444ae7ad2d796ae78c
irgi <irgiaditpratama@gmail.com>
2025-09-23 21:32:14 +0700

    '

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,29 @@
+import React from "react";
+const Button = (props) => {
+  const {children ="holder childern", color ="bg-black", text} = props;
+
+  return (
+    <button
+      className={`${color} font-extrabold text-white px-6 py-2 rounded-full`} type="sumbit" >
+    
+      {text} 
+      {children}
+    </button>
+  );
+};
+
 function App() {
-    return (<div className="flex justify-center underline font-extrabold bg-red-500 min-h-screen items-center">
-     <h1>Hello world!</h1>
-    </div>);
+  return (
+    <div className="flex font-extrabold bg-blue-400 min-h-screen items-center justify-center">
+
+      <div className="flex gap-5">
+        <Button color="bg-amber-700">login</Button>
+        <Button color="bg-red-700">logout</Button>
+        <Button></Button>
+      </div>
+
+    </div>
+  );
 }
 
 export default App;
