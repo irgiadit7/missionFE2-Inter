7206fa6bc6c424fb42abee727f19cd4e2b931580
irgi <irgiaditpratama@gmail.com>
2025-09-23 18:21:15 +0700

    '

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+    return (<div className="flex justify-center underline font-extrabold bg-red-500 min-h-screen items-center">
+     <h1>Hello world!</h1>
+    </div>);
+}
+
+export default App;
